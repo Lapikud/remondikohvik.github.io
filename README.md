e0e82a704df7584ac3489bd5950558b6eec9a7ab
Karina Reisel <kreise@taltech.ee>
2024-08-03 18:02:45 +0300

    leht 0 valmis, timmida ja  tegeleda edasi teiste slaididega.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # remondikohvik.github.io
+
+slaidide üleminek töötab. Lisada värvide üleminekut scrollimisel. vajutamise värvid muutuvad.
+
+Kõik slaidid on lisatud.
+
+leht 0 on valmis.
+
+TEHA:
+Maps teeb overlay mapsist.
+calender viib FB evendile.
--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
+
+        // Remove the class from all anchors
+        document.querySelectorAll('a[href^="#"]').forEach(a => a.classList.remove('highlight'));
+
+        // Add the class to the clicked anchor
+        this.classList.add('highlight');
+
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
+
+//new size new div under 600 px for calender and maps
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 600) {
+        const originalDiv = document.getElementsByClassName('cal_maps');
+        const newDiv = document.getElementById('newDiv_calender_maps');
+        if (originalDiv && newDiv) {
+            newDiv.innerHTML = originalDiv.innerHTML;
+            originalDiv.innerHTML = '';
+        }
+    }
+});
+
+// Trigger the resize event on page load
+window.dispatchEvent(new Event('resize'));
